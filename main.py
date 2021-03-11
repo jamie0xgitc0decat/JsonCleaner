7f39158865ed324acba2607a6f116b579ba29a8a
jamie0xgitc0decat <32829756+jamie0xgitc0decat@users.noreply.github.com>
2021-03-11 13:37:50 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import json 
+
+json_file = ''
+
+def nested_check(field):
+    if isinstance(field, dict):
+        return {key : nested_check(val) for key,val in field.items()}
+    elif isinstance(field, float):
+        return 0.0
+    elif isinstance(field, int):
+        return 0
+    elif isinstance(field, list):
+        return []
+    elif isinstance(field, str):
+        return ""
+    else:
+        return None
+    
+with open(json_file) as file:
+    json_data = {key : nested_loop(val) for key,val in json.load(file).items()}
+
+with open(json_file, 'w') as outfile:
+     json.dump(json_data, outfile, sort_keys = True, indent = 4,
+               ensure_ascii = False)
+
+    
